01353507ab8c242fb30739c5bc990fcfb1e638da
Alican Tarım <alicantarim@hotmail.com>
2024-03-29 23:55:51 +0300

    Initial Commit

---
+++ ShoppingAppSwiftUI/ShoppingAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppSwiftUIApp.swift
+//  ShoppingAppSwiftUI
+//
+//  Created by Alican TARIM on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShoppingAppSwiftUI/Views/CartView.swift
@@ -0,0 +1,42 @@
+//
+//  CartView.swift
+//  ShoppingAppSwiftUI
+//
+//  Created by Alican TARIM on 29.03.2024.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    @EnvironmentObject var cartManager: CartManager
+    var body: some View {
+        ScrollView {
+            if cartManager.products.count > 0 {
+                ForEach(cartManager.products, id: \.id) {
+                    product in
+                    ProductRow(product: product)
+                }
+                
+                HStack {
+                    Text("Your cart total is")
+                    Spacer()
+                    Text("$\(cartManager.total).00")
+                        .bold()
+                }
+                .padding()
+                
+                PaymentButton(action: {})
+                    .padding()
+            } else {
+                Text("Your cart is empty")
+            }
+        }
+        .navigationTitle(Text("My Cart"))
+        .padding(.top)
+    }
+}
+
+#Preview {
+    CartView()
+        .environmentObject(CartManager())
+}
